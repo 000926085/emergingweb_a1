eafad0bc7e453471a03e6db59ec060c570d42db8
tooltime <tooltime@mohawkcollege.ca>
2026-09-22 10:44:47 -0400

    Discord bot initial setup.

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const { clientId, guildId, token } = require('./config.json');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [ GatewayIntentBits.Guilds,
+		         GatewayIntentBits.GuildMessages,
+		         GatewayIntentBits.MessageContent, ],
 });
 
-const server = app.listen(3000, function () {
-    console.log("Example app listening...");
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`)
 });
+
+client.on('interactionCreate', async function(interaction) {
+    if (!interaction.isChatInputCommand()) return;
+
+    // handle your slash command, query your database, reply to the user
+
+    io.emit('botactivity', { command: interaction.commandName, time: new Date() });
+});
+
+client.login(token);
+
+http.listen(3000, function() { console.log('Server listening...'); });
